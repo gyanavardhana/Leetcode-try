9e6691d5de95731a001e8ef12e0b292e52ff9cc4
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-09-27 18:50:07 +0530

    Time: 254 ms (77.4%), Space: 17.4 MB (66.75%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -2,16 +2,21 @@ class MyCalendarTwo:
 
     def __init__(self):
         self.res = []
-        self.over = []
+        self.track = []
+        
+        
     def book(self, start: int, end: int) -> bool:
-        for s,e in self.over:
+        for s,e in self.track:
             if not(end<=s or start>=e):
                 return False
         for s,e in self.res:
             if not(end<=s or start>=e):
-                self.over.append([max(start,s),min(end,e)])
+                self.track.append([max(start,s), min(end,e)])
         self.res.append([start,end])
         return True
+        
+        
+        
         
 
         
