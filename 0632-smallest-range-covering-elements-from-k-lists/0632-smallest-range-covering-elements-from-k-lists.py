420cc28a935d1af65e269b5f351546e0c28706a9
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-10-13 12:46:03 +0530

    Time: 183 ms (87.07%), Space: 23.2 MB (37.22%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -3,21 +3,24 @@ class Solution:
         heap = []
         maxval = float('-inf')
         for i in range(len(nums)):
-            maxval = max(maxval, nums[i][0])
-            heappush(heap, (nums[i][0],i,0))
-        print(heap)
+            maxval = max(maxval,nums[i][0])
+            heappush(heap, (nums[i][0], i, 0))
         res = [heap[0][0], maxval]
         ans = maxval - heap[0][0]
         while True:
             num,row,col = heappop(heap)
-            col+=1
+            col += 1
             if len(nums[row]) == col:
                 return res
-            heappush(heap, (nums[row][col], row, col))
+            heappush(heap, (nums[row][col],row,col))
             maxval = max(maxval, nums[row][col])
             diff = maxval - heap[0][0]
             if ans>diff:
                 ans = diff
                 res = [heap[0][0], maxval]
         return res
+
+        
+
+        
         
