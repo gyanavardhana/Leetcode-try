610ddde35c5ea9115ec7bb07e9f7da8629008524
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-10-13 12:32:48 +0530

    Time: 197 ms (45.99%), Space: 23.2 MB (79.95%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        heap = []
+        maxval = float('-inf')
+        for i in range(len(nums)):
+            maxval = max(maxval, nums[i][0])
+            heappush(heap, (nums[i][0],i,0))
+        print(heap)
+        res = [heap[0][0], maxval]
+        ans = maxval - heap[0][0]
+        while True:
+            num,row,col = heappop(heap)
+            col+=1
+            if len(nums[row]) == col:
+                return res
+            heappush(heap, (nums[row][col], row, col))
+            maxval = max(maxval, nums[row][col])
+            diff = maxval - heap[0][0]
+            if ans>diff:
+                ans = diff
+                res = [heap[0][0], maxval]
+        return res
+        
