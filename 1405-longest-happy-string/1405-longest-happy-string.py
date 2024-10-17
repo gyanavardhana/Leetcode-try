1b7917b3a7717a06167a71f481c70b99caead70c
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-10-17 21:14:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -1,30 +1,19 @@
+import heapq
+
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        heap = []
-        res = []
-        if a>0:
-            heapq.heappush(heap, (-a, "a"))
-        if b>0:
-            heapq.heappush(heap, (-b, "b"))
-        if c>0:
-            heapq.heappush(heap, (-c, "c"))
+        heap, res = [], []
+        for x, ch in [(-a, 'a'), (-b, 'b'), (-c, 'c')]:
+            if x: heapq.heappush(heap, (x, ch))
+        
         while heap:
             c1, ch1 = heapq.heappop(heap)
-            if len(res)>=2 and res[-1]==ch1 and res[-2]==ch1:
-                if not heap:
-                    break
-                c2, ch2 = heapq.heappop(heap)
+            if len(res) > 1 and res[-1] == res[-2] == ch1:
+                if not heap: break
+                c1, ch1, (c2, ch2) = c2, ch2, heapq.heappop(heap)
                 res.append(ch2)
-                c2 += 1
-                if c2<0:
-                    heapq.heappush(heap, (c2, ch2))
-                heapq.heappush(heap, (c1, ch1))
-            else:
-                res.append(ch1)
-                c1+=1
-                if c1<0:
-                    heapq.heappush(heap, (c1,ch1))
+                if c2 + 1: heapq.heappush(heap, (c2 + 1, ch2))
+            res.append(ch1)
+            if c1 + 1: heapq.heappush(heap, (c1 + 1, ch1))
+        
         return ''.join(res)
-        
-
-        
