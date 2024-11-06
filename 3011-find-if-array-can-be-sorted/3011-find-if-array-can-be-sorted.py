dd93ff1b0ae3872de9653105ea655867f8d35bbe
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-11-06 09:53:11 +0530

    Time: 66 ms (21.82%), Space: 16.7 MB (23.35%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -1,19 +1,10 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        def gsb(num):
-            c = 0
-            while num:
-                c += 1
-                num &= num -1
-            return c
-        sbc = []
-        for i in nums:
-            sbc.append(gsb(i))
         swap = True
         while swap:
             swap = False
             for i in range(1,len(nums)):
-                if nums[i]<nums[i-1] and sbc[i] == sbc[i-1]:
+                if nums[i]<nums[i-1] and nums[i].bit_count() == nums[i-1].bit_count():
                     swap = True
                     nums[i-1], nums[i] = nums[i],nums[i-1]
         return sorted(nums) == nums
