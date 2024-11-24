bcd4888347d93b94be5c34c2c8b415380d8c1d58
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-11-24 13:23:39 +0530

    Time: 90 ms (53.85%), Space: 25 MB (41.3%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        su = 0
+        minab = float('inf')
+        negcnt = 0
+        for row in matrix:
+            for x in row:
+                minab = min(minab, abs(x))
+                if x<0:
+                    su += abs(x)
+                    negcnt += 1
+                else:
+                    su += x
+        if negcnt%2==0:
+            return su
+        else:
+            return su-2 * minab
