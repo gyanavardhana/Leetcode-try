9f4bf71a92064a143c5aad74e94f5a2942b40e39
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-12-12 11:02:25 +0530

    Time: 7 ms (49.64%), Space: 17.7 MB (8.46%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        gifts = [-i for i in gifts]
+        heapify(gifts)
+
+    
+        for i in range(k):
+            current = heappop(gifts)
+            current = int(abs(current) ** (1/2))
+
+            heappush(gifts, -current)
+
+        return -1 * sum(gifts)
+        
