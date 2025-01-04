e52df78981756b1856b16eafdbdde248c2ee9371
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2025-01-04 12:19:29 +0530

    Time: 147 ms (79.12%), Space: 22.6 MB (6.37%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,18 +1,17 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        d={}
-        for i,v in enumerate(s):
+        d = {}
+        for i, v in enumerate(s):
             if v in d:
                 d[v].append(i)
             else:
                 d[v] = [i]
         l = []
         for i in d.values():
-            if len(i)>1:
-                l.append([min(i),max(i)])
-        res = 0 
-        for st,en in l:
-            unq = set(s[st+1:en])
+            if len(i) > 1:
+                l.append([min(i), max(i)])
+        res = 0
+        for st, en in l:
+            unq = set(s[st + 1 : en])
             res += len(unq)
         return res
-        
