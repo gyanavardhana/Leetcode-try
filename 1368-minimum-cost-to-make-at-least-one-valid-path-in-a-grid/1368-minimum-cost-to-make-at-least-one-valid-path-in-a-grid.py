edf9483da2b051c02d1a972d20311cfba6e421f5
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2025-01-18 12:46:51 +0530

    Time: 94 ms (89.29%), Space: 19.1 MB (66.88%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,24 +1,24 @@
 from collections import deque
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
-        m,n = len(grid),len(grid[0])
+        m,n = len(grid), len(grid[0])
         dire = {
-            1:[0,1],
-            2:[0,-1],
-            3:[1,0], 
+            1: [0,1],
+            2: [0,-1],
+            3: [1,0],
             4:[-1,0]
         }
         deq = deque([(0,0,0)])
         vis = [[False] * n for i in range(m)]
         while deq:
             cost,x,y = deq.popleft()
-            if(x,y) == (m-1, n-1):
+            if(x,y) == (m-1,n-1):
                 return cost
             if vis[x][y]:
                 continue
             vis[x][y] = True
             for direc, [dx,dy] in dire.items():
-                nx,ny = x+dx,y+dy
+                nx,ny = x + dx, y + dy
                 if 0<=nx<m and 0<=ny<n and not vis[nx][ny]:
                     if grid[x][y] == direc:
                         deq.appendleft((cost,nx,ny))
