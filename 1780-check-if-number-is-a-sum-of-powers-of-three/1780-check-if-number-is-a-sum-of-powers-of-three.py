219625a7c2090b6cb22b4f75132602327584c87c
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2025-03-06 20:24:35 +0530

    Time: 0 ms (100%), Space: 17.9 MB (21.35%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n>0:
+            q, r=divmod(n, 3)
+            if r==2: return False
+            n=q
+        return True
+        
